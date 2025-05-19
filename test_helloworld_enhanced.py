061a97c18762224be8d8ba88ce7964559f224086
Anthonychan <117587945+chanheo2214@users.noreply.github.com>
2025-05-19 23:40:06 +0900

    test_helloworld_enhanced.py

--- test_helloworld_enhanced.py
+++ test_helloworld_enhanced.py
@@ -1,8 +1,10 @@
 import unittest
 from helloworld import say_hello
 
-class HelloWorldTestCase(unittest.TestCase):
-    def test_hello(self):
-        self.assertEqual(say_hello(), "Hello, World!")
+class TestHelloWorld(unittest.TestCase):
+    def test_say_hello(self):
+        result = say_hello()
+        self.assertEqual(result, "Hello, World!")
+
 if __name__ == "__main__":
     unittest.main()
